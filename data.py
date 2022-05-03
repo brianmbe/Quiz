af4456bad5ab8ee3b8759e4472439af091f3a5c4
Brian Nyingambe <99252239+brianmbe@users.noreply.github.com>
2022-05-03 22:18:12 +0300

    update data storage

--- data.py
+++ data.py
@@ -2,10 +2,9 @@ from requests import get
 
 data = get('https://opentdb.com/api.php?amount=20').json()
 
-qn_type = []
 question_data = []
+
 for i in data['results']:
     question_data.append(i)
-    qn_type.append(i['type'])
 
 
